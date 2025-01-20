e994c646f7e204898bd26ce19a1f3b17277e0472
shar <faethychan@gmail.com>
2025-01-20 15:21:45 +0800

    Create Terminal class

---
+++ Terminal.cs
@@ -0,0 +1,52 @@
+﻿class Terminal
+{
+    public string TerminalName { get; set; }
+    public Dictionary<string, Airline> Airlines { get; set; }
+    public Dictionary<string, Flight> Flights { get; set; }
+    public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+    public Dictionary<string, double> GateFees { get; set; }
+
+    public Terminal()
+    {
+        Airlines = new Dictionary<string, Airline>();
+        Flights = new Dictionary<string, Flight>();
+        BoardingGates = new Dictionary<string, BoardingGate>();
+        GateFees = new Dictionary<string, double>();
+    }
+
+    public Terminal(string terminalName, Dictionary<string, Airline> airlines, Dictionary<string, Flight> flights, Dictionary<string, BoardingGate> boardingGates, Dictionary<string, double> gateFees)
+    {
+        TerminalName = terminalName;
+        Airlines = new Dictionary<string, Airline>();
+        Flights = new Dictionary<string, Flight>();
+        BoardingGates = new Dictionary<string, BoardingGate>();
+        GateFees = new Dictionary<string, double>();
+    }
+
+    public bool AddAirline(Airline airline)
+    {
+        return true;
+    }
+
+    public bool AddBoardingGate(BoardingGate boardingGate)
+    {
+        return true;
+    }
+
+    public Airline GetAirlineFromFlight(Flight flight)
+    {
+        return new Airline();
+    }
+
+    public void PrintAirlineFees()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+
+}
