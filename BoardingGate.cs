82913a5b0544f9a765d94d92c72a22cdf2138f83
shar <faethychan@gmail.com>
2025-01-20 15:20:10 +0800

    Create BoardingGate class

---
+++ BoardingGate.cs
@@ -0,0 +1,30 @@
+﻿class BoardingGate
+{
+    public string GetName { get; set; }
+    public bool SupportsCFFT { get; set; }
+    public bool SupportsDDJB { get; set; }
+    public bool SupportsWTT { get; set; }
+    public Flight Flight { get; set; }
+
+    public BoardingGate() { }
+
+    public BoardingGate(string name, bool supportsCFFT, bool supportsDDJB, bool supportsWTT, Flight flight)
+    {
+        GetName = name;
+        SupportsCFFT = supportsCFFT;
+        SupportsDDJB = supportsDDJB;
+        SupportsWTT = supportsWTT;
+        Flight = flight;
+    }
+
+    public double CalculateFees()
+    {
+        return 0.0;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+}
