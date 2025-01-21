f91956dea0809f2cb7293a4aa422fb846cccafaf
Larry Chia <s10259006@connect.np.edu.sg>
2025-01-21 09:23:40 +0800

    Created NORMFlight class

---
+++ NORMFlight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10270189_PRG2Assignment
+{
+    internal class NORMFlight : Flight
+    {
+        public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
+        {
+        }
+
+        public override double CalculateFees()
+        {
+            double totalfee = 0;
+            if (Destination == "Singapore (SIN)")
+            {
+                return totalfee += 500;
+            }
+            else
+            {
+                return totalfee += 800;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Flight Number: {FlightNumber}\nOrigin: {Origin}\nDestination: {Destination}\nExpected Time: {ExpectedTime}\nStatus: {Status}";
+        }
+    }
+}
+
