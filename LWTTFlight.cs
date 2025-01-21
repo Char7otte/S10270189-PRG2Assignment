49b6bfaf629c1f7e693d67891c31f374874e8a31
Larry Chia <s10259006@connect.np.edu.sg>
2025-01-21 09:24:01 +0800

    Create LWTTFlight class

---
+++ LWTTFlight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10270189_PRG2Assignment
+{
+    internal class LWTTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+
+        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = 500;
+        }
+
+        public override double CalculateFees()
+        {
+            double totalfee = 0;
+            if (Destination == "Singapore (SIN)")
+            {
+                return totalfee += 500 + RequestFee;
+            }
+            else
+            {
+                return totalfee += 800 + RequestFee;
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
